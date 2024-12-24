4436fb24c9d5eeb799993cb2244a99708a1642c0
Riera alexandre <alexandre.riera.simplon@gmail.com>
2024-12-24 15:27:19 +0100

    Trying fetch pictures to show equipment details

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -698,55 +698,55 @@ class HomeController extends AbstractController
         switch ($agence) {
             case 'S10':
                 $equipment = $entityManager->getRepository(EquipementS10::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S40':
                 $equipment = $entityManager->getRepository(EquipementS40::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S50':
                 $equipment = $entityManager->getRepository(EquipementS50::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S60':
                 $equipment = $entityManager->getRepository(EquipementS60::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S70':
                 $equipment = $entityManager->getRepository(EquipementS70::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S80':
                 $equipment = $entityManager->getRepository(EquipementS80::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S100':
                 $equipment = $entityManager->getRepository(EquipementS100::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S120':
                 $equipment = $entityManager->getRepository(EquipementS120::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S130':
                 $equipment = $entityManager->getRepository(EquipementS130::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S140':
                 $equipment = $entityManager->getRepository(EquipementS140::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S150':
                 $equipment = $entityManager->getRepository(EquipementS150::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S160':
                 $equipment = $entityManager->getRepository(EquipementS160::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             case 'S170':
                 $equipment = $entityManager->getRepository(EquipementS170::class)->findOneBy(['id' => $id]);
-                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement(), 'raison_sociale_visite' => $equipment->getRaisonSociale()]);
+                $picturesArray = $entityManager->getRepository(Form::class)->findBy(['code_equipement' => $equipment->getNumeroEquipement()]);
                 break;
             
             default:
