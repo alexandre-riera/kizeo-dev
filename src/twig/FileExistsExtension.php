2f42a0e35abe7ac6d777eddf8c77a4c68ccd5a9c
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-01-21 08:34:32 +0100

    [FIXING] Trying years array in front to scan directory if pdf exist into this directory

--- src/twig/FileExistsExtension.php
+++ src/twig/FileExistsExtension.php
@@ -10,7 +10,7 @@ class FileExistsExtension extends AbstractExtension
     public function getFilters(): array
     {
         return [
-            new TwigFilter('fileExists', [$this, 'fileExists']),
+            new TwigFilter('file_exists', [$this, 'file_exists']),
         ];
     }
 
