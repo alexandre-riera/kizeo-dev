c95d4693b631be317719f129e3c392f848e50e81
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-01-23 22:51:18 +0100

    [FEATURE] DOING update Kizeo by BDD

--- src/Repository/FormRepository.php
+++ src/Repository/FormRepository.php
@@ -849,6 +849,7 @@ class FormRepository extends ServiceEntityRepository
         dump(count($kizeoEquipmentsGrenoble));
         foreach ($structuredEquipementsGrenoble as $structuredEquipment) {
             $clientVisiteCodeEquipement = preg_split('/[|]/',$structuredEquipment);
+            dd($clientVisiteCodeEquipement);
             $keyEquipment = array_search($clientVisiteCodeEquipement[0], $kizeoEquipmentsGrenoble);
             if (str_contains($kizeoEquipmentsGrenoble[$keyEquipment], $clientVisiteCodeEquipement[0])) {
                 dump($structuredEquipment);
