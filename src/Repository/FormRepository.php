4b58c1c9e0dcbded27aac63927d914af175d7db6
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-02-03 09:33:10 +0100

    [FEATURE] Updating KIZEO with BDD data

--- src/Repository/FormRepository.php
+++ src/Repository/FormRepository.php
@@ -813,7 +813,7 @@ class FormRepository extends ServiceEntityRepository
         foreach ($structuredEquipementsSplitted as $index => $equipmentSplitted) {
             foreach ($kizeoEquipments as $key => $kizeoEquipment) {
                 dump($kizeoEquipment);
-                dump($structuredEquipementsSplitted[0]);
+                dd($structuredEquipementsSplitted[0]);
                 if (str_starts_with($kizeoEquipment, $structuredEquipementsSplitted[0])) {
                     $kizeoEquipments[] = $structuredEquipements[$index -= 1]; // Ajouter l'équipement à la liste Kizeo
                     unset($kizeoEquipments[$key]); // Supprimer l'équipement de la liste Kizeo s'il existe déjà
