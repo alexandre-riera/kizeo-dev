ca26947fe9f29344096c26071275802ae232ab9a
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-02-04 09:36:12 +0100

    [FEATURE] Updating KIZEO with BDD data

--- src/Repository/FormRepository.php
+++ src/Repository/FormRepository.php
@@ -871,7 +871,7 @@ class FormRepository extends ServiceEntityRepository
                 }
             }
         }
-        dd(count($newListKizeoTopload));
+        dd(count($kizeoEquipments));
         $kizeoEquipments = $newListKizeoTopload;
     }
     /**
