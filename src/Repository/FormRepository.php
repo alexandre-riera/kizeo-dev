bd3ca8b2a752b46e4055e19f1875300ecdd291e3
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-02-19 10:59:48 +0100

    [MAJ KIZEO WITH BDD] Dumping variables from comparerEtMettreAJourListeKizeo function

--- src/Repository/FormRepository.php
+++ src/Repository/FormRepository.php
@@ -916,7 +916,7 @@ class FormRepository extends ServiceEntityRepository
         $elementsToAdd = array_diff($structuredEquipements, $kizeoEquipments);
         $elementsToRemove = array_diff($kizeoEquipments, $structuredEquipements);
         dump($elementsToAdd);
-        dd($elementsToRemove);
+        dump($elementsToRemove);
         // Update Kizeo Equipments
         $updatedKizeoEquipments = array_merge(array_diff($kizeoEquipments, $elementsToRemove), $elementsToAdd);
 
