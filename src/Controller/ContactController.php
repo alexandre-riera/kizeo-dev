a2c17c7056e2bdc73b5de8e85464aabcd41cc77a
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-02-26 11:07:41 +0100

    [Feature][Page new client]

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -105,7 +105,7 @@ class ContactController extends AbstractController
             
         //     return $this->redirectToRoute('app_contact_new');
         // }
-        dd($contactsFromKizeo);
+        dump($contactsFromKizeo);
 
         return $this->render('contact/index.html.twig', [
             'agences' => $agences,
