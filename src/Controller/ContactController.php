1794a43c4d36073aeea2b2b10e54c0639463c5ea
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-03-03 16:35:28 +0100

    [Feature][Page update client] Get contact data from form and format to string before upload to kizeo contact list

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Service\KizeoService;
-use stdClass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
