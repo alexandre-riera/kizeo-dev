c994515382c02f97aee6ed1b9d894e154766361b
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-03-06 10:12:58 +0100

    [CONTRAT] show selectedInformations in table

--- src/Controller/ContratController.php
+++ src/Controller/ContratController.php
@@ -242,7 +242,7 @@ class ContratController extends AbstractController
                     break;
             }
         }
-        dd($clientSelectedInformations);
+        dump($clientSelectedInformations);
 
         return $this->render('contrat/index.html.twig', [
             'agences' => $agences,
