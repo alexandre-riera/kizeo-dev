e7bfa683aada2be5d6efa0b7aad1328212fa23ce
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-03-06 10:02:47 +0100

    [CONTRAT] show selectedInformations in table

--- src/Controller/ContratController.php
+++ src/Controller/ContratController.php
@@ -130,7 +130,7 @@ class ContratController extends AbstractController
 
         $clientSelectedInformations = "";
 
-        if (!empty($_POST['clientName'])) {
+        // if ($contactSelectionne != "") {
             switch ($agenceSelectionnee) {
                 case 'S10':
                     $clientSelectedInformations  =  $entityManager->getRepository(ContactS10::class)->findOneBy(['id_contact' => $contactId]);
@@ -241,7 +241,7 @@ class ContratController extends AbstractController
                     
                     break;
             }
-        }
+        // }
 
         return $this->render('contrat/index.html.twig', [
             'agences' => $agences,
