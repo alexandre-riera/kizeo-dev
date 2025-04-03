de5d59e79cf52d728471a340eae527f99c55a3ce
unknown <Alex@PC-ARI.somafi.lan>
2025-04-03 09:47:43 +0200

    ADD Script to rename kizeo pdf OK | BEGIN Update show pdf in front

--- src/Repository/HomeRepository.php
+++ src/Repository/HomeRepository.php
@@ -50,7 +50,7 @@ class HomeRepository{
         $results = [];
         foreach ($yearsArray as $year) {
             dump($year);
-            if(is_dir("https://www.pdf.somafi-group.fr/" . $agenceSelected . "/" . $clientSelected . "/" . $year . "/" .$visite)){
+            if(is_dir("https://www.pdf.somafi-group.fr/" . $agenceSelected . "/" . $clientSelected . "/" . $year )){
                 $directoriesLists = scandir( "https://www.pdf.somafi-group.fr/" . $agenceSelected . "/" . $clientSelected . "/" . $year . "/" .$visite );
                 foreach($directoriesLists as $fichier){
 
