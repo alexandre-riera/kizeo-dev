7eb5ec0faed47dfa6e3343d6430dc7a3795e5e8e
unknown <Alex@PC-ARI.somafi.lan>
2025-04-03 09:57:35 +0200

    ADD Script to rename kizeo pdf OK | BEGIN Update show pdf in front

--- src/Repository/HomeRepository.php
+++ src/Repository/HomeRepository.php
@@ -38,35 +38,66 @@ class HomeRepository{
         return $listClientsFiltered;
     }
 
+    // public function getListOfPdf($clientSelected, $visite, $agenceSelected){
+    //     // I add 2024 in the url cause we are in 2025 and there is not 2025 folder yet
+    //     // MUST COMPLETE THIS WITH 2024 AND 2025 TO LIST PDF FILES IN FOLDER
+    //     $yearsArray = [2024, 2025, 2026, 2027, 2028,2029, 2030];
+    //     $agenceSelected = trim($agenceSelected);
+    //     $clientSelected = str_replace(" ", "_", $clientSelected);
+    //     dump($clientSelected);
+    //     dump($agenceSelected);
+    //     dump($visite);
+    //     $results = [];
+    //     foreach ($yearsArray as $year) {
+    //         // dump($year);
+    //         if(is_dir("https://www.pdf.somafi-group.fr/" . $agenceSelected . "/" . $clientSelected . "/" . $year . "/" .$visite)){
+    //             $directoriesLists = scandir( "https://www.pdf.somafi-group.fr/" . $agenceSelected . "/" . $clientSelected . "/" . $year . "/" .$visite );
+    //             foreach($directoriesLists as $fichier){
+
+    //                 if(preg_match("#\.(pdf)$#i", $fichier)){
+                        
+    //                     $myFile = new stdClass;
+    //                     $myFile->path = $fichier;
+    //                     $myFile->annee = $year;
+    //                     //la preg_match définie : \.(jpg|jpeg|png|gif|bmp|tif)$
+                        
+    //                     //Elle commence par un point "." (doit être échappé avec anti-slash \ car le point veut dire "tous les caractères" sinon)
+                        
+    //                     //"|" parenthèses avec des barres obliques dit "ou" (plusieurs possibilités : jpg ou jpeg ou png...)
+                        
+    //                     //La condition "$" signifie que le nom du fichier doit se terminer par la chaîne spécifiée. Par exemple, un fichier nommé 'monFichier.jpg.php' ne sera pas accepté, car il ne se termine pas par '.jpg', '.jpeg', '.png' ou toute autre extension souhaitée.
+                        
+    //                     if (!in_array($myFile, $results)) {
+    //                         array_push($results, $myFile);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+        
+    //     return $results;
+    // }
     public function getListOfPdf($clientSelected, $visite, $agenceSelected){
-        // I add 2024 in the url cause we are in 2025 and there is not 2025 folder yet
-        // MUST COMPLETE THIS WITH 2024 AND 2025 TO LIST PDF FILES IN FOLDER
         $yearsArray = [2024, 2025, 2026, 2027, 2028,2029, 2030];
         $agenceSelected = trim($agenceSelected);
         $clientSelected = str_replace(" ", "_", $clientSelected);
-        dump($clientSelected);
-        dump($agenceSelected);
-        dump($visite);
+        
         $results = [];
+        
         foreach ($yearsArray as $year) {
-            // dump($year);
-            if(is_dir("https://www.pdf.somafi-group.fr/" . $agenceSelected . "/" . $clientSelected . "/" . $year . "/" .$visite)){
-                $directoriesLists = scandir( "https://www.pdf.somafi-group.fr/" . $agenceSelected . "/" . $clientSelected . "/" . $year . "/" .$visite );
-                foreach($directoriesLists as $fichier){
-
-                    if(preg_match("#\.(pdf)$#i", $fichier)){
-                        
+            $remotePath = "https://www.pdf.somafi-group.fr/" . $agenceSelected . "/" . $clientSelected . "/" . $year . "/" . $visite;
+    
+            // Use file_get_contents to get the directory listing from the remote server
+            $contents = file_get_contents($remotePath);
+    
+            if ($contents !== false) {
+                // Explode the contents by newline to get individual files/folders
+                $files = explode("\n", $contents);
+                foreach ($files as $file) {
+                    if (preg_match("#\.(pdf)$#i", $file)) {
                         $myFile = new stdClass;
-                        $myFile->path = $fichier;
+                        $myFile->path = $file;
                         $myFile->annee = $year;
-                        //la preg_match définie : \.(jpg|jpeg|png|gif|bmp|tif)$
-                        
-                        //Elle commence par un point "." (doit être échappé avec anti-slash \ car le point veut dire "tous les caractères" sinon)
-                        
-                        //"|" parenthèses avec des barres obliques dit "ou" (plusieurs possibilités : jpg ou jpeg ou png...)
-                        
-                        //La condition "$" signifie que le nom du fichier doit se terminer par la chaîne spécifiée. Par exemple, un fichier nommé 'monFichier.jpg.php' ne sera pas accepté, car il ne se termine pas par '.jpg', '.jpeg', '.png' ou toute autre extension souhaitée.
-                        
                         if (!in_array($myFile, $results)) {
                             array_push($results, $myFile);
                         }
@@ -74,7 +105,7 @@ class HomeRepository{
                 }
             }
         }
-        
+    
         return $results;
     }
 }
