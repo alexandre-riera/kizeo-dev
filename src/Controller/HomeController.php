c00002356cf08dc8cc921baa8dfdbbed998d20a6
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-04-04 10:07:52 +0200

    FIX trim

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -163,9 +163,11 @@ class HomeController extends AbstractController
         if(isset($_POST['submitClient'])){  
             if(!empty($_POST['clientName'])) {  
                 $clientSelected = $_POST['clientName'];
-                dd($clientSelected);
+                dump($clientSelected);
                 $agenceSelected = substr($clientSelected, -4);
+                dump($agenceSelected);
                 $agenceSelected = trim($agenceSelected);
+                dump($agenceSelected);
             } else {  
                 echo 'Please select the value.';
             }  
@@ -173,12 +175,16 @@ class HomeController extends AbstractController
         
         // ENLEVER LE NOM DE L'AGENCE ET L'ESPACE A LA FIN DU NOM DU CLIENT SÉLECTIONNÉ
         $clientSelectedRTrimmed = rtrim($clientSelected, "\S10\S40\S50\S60\S70\S80\S100\S120\S130\S140\S150\S160\S170\ \-");
+        dump($clientSelectedRTrimmed);
         $clientSelectedSplitted = preg_split("/[-]/",$clientSelectedRTrimmed);
+        dump($clientSelectedSplitted);
         $idClientSelected = $clientSelectedSplitted[0];
         foreach ($clientSelectedSplitted as $key) {
             $clientSelected = $key;
         }
+        dump($clientSelected);
         $idClientSelected = rtrim($idClientSelected, "\ ");
+        dump($idClientSelected);
         $visiteDuClient = "";
         if ($clientSelected != NULL) {
             switch ($agenceSelected) {
