fce60ac43471a56a84898fde21fbe755d9285684
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-04-09 15:04:35 +0200

    UPDATE database from forms V1 to V5

--- src/Repository/FormRepository.php
+++ src/Repository/FormRepository.php
@@ -101,7 +101,7 @@ class FormRepository extends ServiceEntityRepository
                 $formMaintenanceArray [] = $form;
             }
         }
-        dd($formMaintenanceArray);
+        
         return $formMaintenanceArray;
     }
     
