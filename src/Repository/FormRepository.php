14ae1bd4d9b5cb4663318e7c9bf426ff32cfe18f
unknown <Alex@PC-ARI.somafi.lan>
2025-04-10 18:09:35 +0200

    UPDATE ALL EQUIPMENTS INFORMATIONS IN DATABASE

--- src/Repository/FormRepository.php
+++ src/Repository/FormRepository.php
@@ -103,7 +103,7 @@ class FormRepository extends ServiceEntityRepository
                 $formMaintenanceArray [] = $form;
             }
         }
-
+        dd($formMaintenanceArray);
         foreach ($formMaintenanceArray as $formMaintenance) {
             $response = $this->client->request('POST', 
                 'https://forms.kizeo.com/rest/v3/forms/' . $formMaintenance['id'] . '/data/advanced', [
