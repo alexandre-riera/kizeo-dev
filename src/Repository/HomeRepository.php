f40aadd6d51c9157cd94013560095ec1e45b45dc
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-04-18 09:23:33 +0200

    UPDATE GET contacts lists from kizeo instead from cache cause issues

--- src/Repository/HomeRepository.php
+++ src/Repository/HomeRepository.php
@@ -37,7 +37,7 @@ class HomeRepository{
                 if(isset($clientFiltered[8])) {
                     array_push($listClientsFiltered, $clientFiltered[6] . "-" . $clientFiltered[0] . " - " . $clientFiltered[8]);
                 }else{
-                    array_push($listClientsFiltered, $clientFiltered[6] . "-" . $clientFiltered[0] . " - " . $clientFiltered[9]);
+                    array_push($listClientsFiltered, $clientFiltered[6] . "-" . $clientFiltered[0]);
                 }
             }
         }
