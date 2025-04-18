340759c3848ee34890306eafc9fbc10d7b73f3c4
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-04-18 10:17:17 +0200

    UPDATE GET contacts lists from kizeo instead from cache cause issues

--- src/Repository/HomeRepository.php
+++ src/Repository/HomeRepository.php
@@ -26,13 +26,13 @@ class HomeRepository{
         );
         $content = $response->getContent();
         $content = $response->toArray();
-        if (isset($content['list']['items'])) {
+        // if (isset($content['list']['items'])) {
             $content = $content['list']['items'];
             foreach ($content as $client) {
                 $modifiedClient = preg_split("/[:|]/",$client);
                 $listSplitted[] = $modifiedClient;
             }
-        }
+        // }
         $listSplitted = [];
         $listClientsFiltered = [];
         // if (isset($listSplitted)) {
