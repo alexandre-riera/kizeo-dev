f2ae890d3bb476f4912e9f94aaa4c3f048333e7e
unknown <Alex@PC-ARI.somafi.lan>
2025-04-26 22:31:50 +0200

    UPDATE filters

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1614,7 +1614,7 @@ class HomeController extends AbstractController
     }
 
     #[Route('/save/modal/equipement', name: 'app_save_modal_equipement')]
-    public function saveModalInDatabase(EntityManagerInterface $entityManager, Request $request): void
+    public function saveModalInDatabase(EntityManagerInterface $entityManager, Request $request): Response
     {
 
         // Récupération de l'équipement édité dans la modal
@@ -1748,7 +1748,7 @@ class HomeController extends AbstractController
             $entityManager->flush();
         }
         $this->addFlash('success', 'L\'équipement a été mit à jour avec succès !');
-        // return new Response("L'équipement édité dans la modal a bien été enregistré en base de données", Response::HTTP_OK, [], true);
+        return new Response("<html><body><p  style='font-size:18px; font-weight:bold;'>L'équipement édité dans la modal a bien été enregistré en base de données</p></body></html>", Response::HTTP_OK, [], true);
         // return $this->redirect($request->getUri());
     }
 
