1950ffb1e4202c7b4747175db1df134d34245fac
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-06-10 14:07:44 +0200

    UPDATE database properties photo_compte_rendu for Form table and boolean is_archive for equipments tables

--- src/Controller/EquipementPdfController.php
+++ src/Controller/EquipementPdfController.php
@@ -198,7 +198,7 @@ class EquipementPdfController extends AbstractController
             if ($equipement['equipment']->getEtat() === "Equipement non présent sur site" || $equipement['equipment']->getEtat() === "G") {
                 $equipementsNonPresents[] = $equipement;
             }
-            $dateDeDerniererVisite = $equipement['equipment']->getDateDerniereVisite();
+            $dateDeDerniererVisite = $equipement['equipment']->getDerniereVisite();
         }
 
         // Générer le HTML pour le PDF
