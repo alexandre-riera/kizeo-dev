6f985ef88037b8addfaa12fb0f9919a40aa64585
alexandre-riera <alexandre.riera.dev@gmail.com>
2025-07-18 01:11:52 +0200

    ADD Gestion par agence de l'application

--- src/Controller/SimplifiedMaintenanceController.php
+++ src/Controller/SimplifiedMaintenanceController.php
@@ -3647,7 +3647,6 @@ class SimplifiedMaintenanceController extends AbstractController
             }
             
             dump("===== FIN TRAITEMENT SOUMISSION " . $submission['entry_id'] . " =====");
-            die("Traitement de la soumission {$submission['entry_id']} terminé");
         } catch (\Exception $e) {
             $errors++;
             dump("Erreur traitement soumission {$submission['entry_id']}: " . $e->getMessage());
