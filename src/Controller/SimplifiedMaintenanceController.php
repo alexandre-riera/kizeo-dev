a61f6199f9eb56623b8fdf99d85ea76ccd02609f
alexandre-riera <alexandre.riera.dev@gmail.com>
2025-07-20 19:51:25 +0200

    ADD Gestion par agence de l'application

--- src/Controller/SimplifiedMaintenanceController.php
+++ src/Controller/SimplifiedMaintenanceController.php
@@ -3691,9 +3691,9 @@ class SimplifiedMaintenanceController extends AbstractController
         dump("Numéro formaté final: '" . $numeroFormate . "'");
         
         // 2. Vérifier si l'équipement existe déjà (même si c'est un nouveau numéro, vérifier par autres critères)
-        // if ($this->offContractEquipmentExists($equipmentHorsContrat, $idClient, $entityClass, $entityManager)) {
-        //     return false; // Skip car déjà existe
-        // }
+        if ($this->offContractEquipmentExists($equipmentHorsContrat, $idClient, $entityClass, $entityManager)) {
+            return false; // Skip car déjà existe
+        }
         
         // 3. Définir les données de l'équipement hors contrat
         $equipement->setNumeroEquipement($numeroFormate);
