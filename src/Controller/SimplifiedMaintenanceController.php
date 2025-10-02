9dc0c903eec5aa6cdff149342dbdefc4575225a7
alexandre-riera <alexandre.riera.dev@gmail.com>
2025-10-02 22:41:30 +0200

    DELETE timeout verification line 4005

--- src/Controller/SimplifiedMaintenanceController.php
+++ src/Controller/SimplifiedMaintenanceController.php
@@ -3910,7 +3910,7 @@ class SimplifiedMaintenanceController extends AbstractController
         }
         // Configuration conservative
         ini_set('memory_limit', '3G');
-        ini_set('max_execution_time', 600);
+        ini_set('max_execution_time', 1800);
         
         $validAgencies = ['S10', 'S40', 'S50', 'S60', 'S70', 'S80', 'S100', 'S120', 'S130', 'S140', 'S150', 'S160', 'S170'];
         
